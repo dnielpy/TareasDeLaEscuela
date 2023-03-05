9935a57193b84bbb567124632fa8c732e105d3a4
dnielpy <dnielquesada@gmail.com>
2023-03-05 23:03:37 +0100

    Agregado algunos ejercicios

---
+++ 03 - Velocidad de Escritura.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main(){
+        // Implemente una funcion que cuando se invoque, espere una entrada´ de texto y devuelva la velocidad de escritura del usuario.
+        static double writeSpeed(){
+            Stopwatch crono = new Stopwatch();
+            Console.WriteLine("Escribe un texto: ");
+
+            crono.Start();
+            string texto = Console.ReadLine();
+            crono.Stop();
+
+            double ms = (double) crono.ElapsedMilliseconds;
+
+            return ms;
+        }
+
+        Console.WriteLine($"Te has demorado: {writeSpeed()} ms");
+
+    
+    }
+}
---
+++ 04 - Año Biciesto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main(){
+        static void biciesto(int x){
+            if (x % 4 == 0 && x % 100 != 0)
+            {
+                Console.WriteLine("El año es biciesto");
+                Environment.Exit(0);
+            }
+            if (x % 4 == 0 && x % 100 == 0 && x % 400 == 0)
+            {
+                Console.WriteLine("El año es biciesto");
+                Environment.Exit(0);    //Para finalizar el programa
+            }
+            else
+            {    
+                Console.WriteLine("El año no es biciesto");
+            }
+        }
+        System.Console.WriteLine("Escriba el año: ");
+        int year = int.Parse(Console.ReadLine());
+        biciesto(year);
+        
+    
+    }
+}
