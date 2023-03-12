6a71b41b89502e2204942e78563875f952370578
dnielpy <dnielquesada@gmail.com>
2023-03-12 13:07:33 -0500

    Organizado

---
+++ 05 - Bucles/01 - Mayor de un array.cs
@@ -0,0 +1,18 @@
+using System;
+class Program
+{
+    static void Main(){
+        //Imlementar un metodo que devuelva el mayor de un array
+        static int Mayor(int[] array){
+            int maxValue = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                maxValue = Math.Max(array[i], maxValue);
+            }
+            System.Console.WriteLine(maxValue);
+            return 0;
+        }
+        int[] miArray = {1,20,3,4,5};
+        Mayor(miArray);
+    }
+}
